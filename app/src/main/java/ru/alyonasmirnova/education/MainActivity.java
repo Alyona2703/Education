a5244e3f275a5c698c5bc0b7023bd53242982044
Alyona2703 <alyona27962mail.ru>
2020-10-10 09:43:42 +0300

    That's all

---
+++ app/src/main/java/ru/alyonasmirnova/education/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.alyonasmirnova.education;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Registration(View view) {
+
+    }
+}
